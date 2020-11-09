5d35a9eee36b98bc87f6f5ca914b6aac51cf8bbf
emanuelmilotic56 <58472080+emanuelmilotic56@users.noreply.github.com>
2020-11-09 09:27:43 +0100

    Add files via upload

---
+++ Varijable vježbe 3/Varijable vježbe 3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Varijable_vježbe_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 2;
+            double b = 2;
+            double c = 2;
+            double d = 2;
+            double x;
+            x = (Math.Pow(a, 2) + Math.Pow(b, 2) + Math.Pow(c, 2) + Math.Pow(d, 2)) / (Math.Pow(a + b, 2) + Math.Pow(c + d, 2));
+            Console.Write(x);
+            
+
+
+        }
+    }
+}
